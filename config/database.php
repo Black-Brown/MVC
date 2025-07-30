c1098b3098bcdf81bed777ec94000e98c7955fa6
Jheinel-Brown <109928519+Black-Brown@users.noreply.github.com>
2025-07-30 15:39:46 -0400

    config database

---
+++ config/database.php
@@ -0,0 +1,22 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'school_db');   
+
+class database {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        } else {
+            echo "Connected successfully";
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
